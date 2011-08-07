15e8c6297ec3483d1e130b4ab2f9eb85e69e6dc0
blackfoks <mail@blackfoks.com>
2011-08-07 19:53:19 +0600

    Samurai and SamuraiOptions class added

---
+++ Samurai/Samurai.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samurai
+{
+    /// <summary>
+    /// Represents main API.
+    /// </summary>
+    public static class Samurai
+    {
+        public static readonly string DefaultSite = "https://samurai.feefighters.com/v1/";
+        public static readonly SamuraiOptions DefaultOptions = new SamuraiOptions() { Site = DefaultSite };
+
+        private static SamuraiOptions _options = new SamuraiOptions();
+
+        /// <summary>
+        /// Gets or sets connection options.
+        /// </summary>
+        public static SamuraiOptions Options
+        {
+            get { return _options; }
+            set
+            {
+                _options = (value ?? new SamuraiOptions()).ReverseMerge(DefaultOptions);
+                // Samurai::Base.setup_site!
+            }
+        }
+    }
+}
---
+++ Samurai/SamuraiOptions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samurai
+{
+    /// <summary>
+    /// Represents connection options.
+    /// </summary>
+    public class SamuraiOptions
+    {
+        /// <summary>
+        /// Gets or sets API base url.
+        /// </summary>
+        public string Site { get; set; }
+
+        /// <summary>
+        /// Gets or sets merchant key for connection.
+        /// </summary>
+        public string MerchantKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets merchant password for connection.
+        /// </summary>
+        public string MerchantPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets processor token for connection.
+        /// </summary>
+        public string ProcessorToken { get; set; }
+
+        /// <summary>
+        /// Merges this options with another. Properties of this options which are not null
+        /// will not be merged (overrided).
+        /// </summary>
+        /// <param name="options">Options to merge with.</param>
+        /// <returns>this options merged with another.</returns>
+        public SamuraiOptions ReverseMerge(SamuraiOptions options)
+        {
+            Site = Site ?? options.Site;
+            MerchantKey = MerchantKey ?? options.MerchantKey;
+            MerchantPassword = MerchantPassword ?? options.MerchantPassword;
+            ProcessorToken = ProcessorToken ?? options.ProcessorToken;
+
+            return this;
+        }
+    }
+}
