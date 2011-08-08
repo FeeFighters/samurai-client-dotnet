847e695f154bfedddf0b989ad77abbb2178bea0d
blackfoks <mail@blackfoks.com>
2011-08-08 17:07:54 +0600

    Processor and PurchaseXmlPayload added. Simple purchase added.

---
+++ Samurai/Processor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace Samurai
+{
+    public class Processor : SamuraiBase
+    {
+        public Processor(string processorToken)
+        {
+            ProcessorToken = processorToken;
+        }
+
+        public static Processor TheProcessor
+        {
+            get { return new Processor(Samurai.ProcessorToken); }
+        }
+
+        public string ProcessorToken { get; private set; }
+
+        /// <summary>
+        /// Most generic form of simple purchase.
+        /// </summary>
+        /// <param name="paymentMethodToken"></param>
+        /// <param name="amount"></param>
+        /// <param name="descriptor"></param>
+        /// <param name="custom"></param>
+        /// <param name="customer_reference"></param>
+        /// <param name="billing_reference"></param>
+        /// <returns></returns>
+        public Transaction Purchase(string paymentMethodToken, string amount, string descriptor = null,
+            string custom = null, string customer_reference = null, string billing_reference = null)
+        {
+            // Create request
+            var request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Xml;
+            request.Resource = "processors/{ProcessorToken}/purchase.xml";
+            request.RootElement = "transaction";
+
+            // Set processor token
+            request.AddParameter("ProcessorToken", ProcessorToken, ParameterType.UrlSegment);
+
+            // Generate payload
+            request.AddBody(new PurchaseXmlPayload()
+            {
+                Type = "purchase",
+                Amount = amount,
+                CurrencyCode = "USD",
+                PaymentMethodToken = paymentMethodToken,
+                Descriptor = descriptor ?? string.Empty,
+                Custom = custom ?? string.Empty,
+                CustomerReference = customer_reference ?? string.Empty,
+                BillingReference = billing_reference ?? string.Empty
+            });
+
+            // Send a request and deserialize response into transaction
+            return Execute<Transaction>(request);
+        }
+    }
+}
---
+++ Samurai/XmlPayloads.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp.Serializers;
+
+namespace Samurai
+{
+    [SerializeAs(Name = "transaction")]
+    class PurchaseXmlPayload
+    {
+        public PurchaseXmlPayload()
+        {
+            CurrencyCode = "USD";
+        }
+
+        /// <summary>
+        /// Gets or sets type for transaction.
+        /// </summary>
+        [SerializeAs(Name = "type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets amount to authorize.
+        /// </summary>
+        [SerializeAs(Name = "amount")]
+        public string Amount { get; set; }
+
+        /// <summary>
+        /// Gets or sets currency code, default is USD.
+        /// </summary>
+        [SerializeAs(Name = "currency_code")]
+        public string CurrencyCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets token identifying the payment method to authorize.
+        /// </summary>
+        [SerializeAs(Name = "payment_method_token")]
+        public string PaymentMethodToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets descriptor for the transaction.
+        /// </summary>
+        [SerializeAs(Name = "descriptor")]
+        public string Descriptor { get; set; }
+
+        /// <summary>
+        /// Gets or sets custom data, this data does not get passed to the processor, 
+        /// it is stored within samurai.feefighters.com only.
+        /// </summary>
+        [SerializeAs(Name = "custom")]
+        public string Custom { get; set; }
+
+        /// <summary>
+        /// Gets or sets an identifier for the customer, 
+        /// this will appear in the processor if supported.
+        /// </summary>
+        [SerializeAs(Name = "customer_reference")]
+        public string CustomerReference { get; set; }
+
+        /// <summary>
+        /// Gets or sets an identifier for the purchase, 
+        /// this will appear in the processor if supported.
+        /// </summary>
+        [SerializeAs(Name = "billing_reference")]
+        public string BillingReference { get; set; }
+    }
+}
