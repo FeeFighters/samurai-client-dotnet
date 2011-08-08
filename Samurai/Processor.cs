c1f6a14be064746f15dc333d5cd2b1fd3bd5ea2d
blackfoks <mail@blackfoks.com>
2011-08-08 21:41:30 +0600

    Authorization added

--- Samurai/Processor.cs
+++ Samurai/Processor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using RestSharp;
+using System.Xml.Linq;
 
 namespace Samurai
 {
@@ -46,6 +47,45 @@ namespace Samurai
             request.AddBody(new PurchaseXmlPayload()
             {
                 Type = "purchase",
+                Amount = amount,
+                CurrencyCode = "USD",
+                PaymentMethodToken = paymentMethodToken,
+                Descriptor = descriptor ?? string.Empty,
+                Custom = custom ?? string.Empty,
+                CustomerReference = customer_reference ?? string.Empty,
+                BillingReference = billing_reference ?? string.Empty
+            });
+
+            // Send a request and deserialize response into transaction
+            return Execute<Transaction>(request);
+        }
+
+        /// <summary>
+        /// Authorizes a payment_method for a particular amount. 
+        /// </summary>
+        /// <param name="paymentMethodToken"></param>
+        /// <param name="amount"></param>
+        /// <param name="descriptor"></param>
+        /// <param name="custom"></param>
+        /// <param name="customer_reference"></param>
+        /// <param name="billing_reference"></param>
+        /// <returns></returns>
+        public Transaction Authorize(string paymentMethodToken, string amount, string descriptor = null,
+            string custom = null, string customer_reference = null, string billing_reference = null)
+        {
+            // Create request
+            var request = new RestRequest(Method.POST);
+            request.RequestFormat = DataFormat.Xml;
+            request.Resource = "processors/{ProcessorToken}/authorize.xml";
+            request.RootElement = "transaction";
+
+            // Set processor token
+            request.AddParameter("ProcessorToken", ProcessorToken, ParameterType.UrlSegment);
+
+            // Generate payload
+            request.AddBody(new PurchaseXmlPayload()
+            {
+                Type = "authorize",
                 Amount = amount,
                 CurrencyCode = "USD",
                 PaymentMethodToken = paymentMethodToken,
