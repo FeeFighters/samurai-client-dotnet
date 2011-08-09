51c126638d8d51db6cf21f4222059989a77ff972
blackfoks <mail@blackfoks.com>
2011-08-09 09:58:10 +0600

    Helper class added with method for converting decimals to string in necessary format

---
+++ Samurai/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samurai
+{
+    public static class Helper
+    {
+        public static string DecimalToString(decimal value)
+        {
+            return value.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
