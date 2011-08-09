b0e122a916c46f68505d1b5f2ac8af142bef1607
blackfoks <mail@blackfoks.com>
2011-08-09 12:37:32 +0600

    Purchase tests added

--- Samurai.Test/PurchaseTest.cs
+++ Samurai.Test/PurchaseTest.cs
@@ -9,7 +9,17 @@ namespace Samurai.Test
     [TestClass]
     public class PurchaseTest
     {
-        private Processor processor;
+        private Processor _processor;
+        private string _paymentMethodToken;
+
+        private string _testDescriptor
+        {
+            get { return string.Format("Descriptor for testing purposes at {0}", DateTime.UtcNow); }
+        }
+        private string _testCustom
+        {
+            get { return string.Format("Custom data for testing purposes at {0}", DateTime.UtcNow); }
+        }
 
         [TestInitialize]
         public void TestInitialize()
@@ -20,7 +30,9 @@ namespace Samurai.Test
                 MerchantPassword = "cde50d5324038ddc69415050",
                 ProcessorToken = "b0d9c4324dabd84975b0a5e1"
             };
-            processor = Processor.TheProcessor;
+
+            _processor = Processor.TheProcessor;
+            _paymentMethodToken = "11162477aad1a7053b72dbd0";
         }
 
         [TestCleanup]
@@ -30,11 +42,60 @@ namespace Samurai.Test
         }
 
         [TestMethod]
-        public void SimplePurchase120USD()
+        public void Simple_Purchase_17_USD_As_String()
         {
-            var trans = processor.Purchase("11162477aad1a7053b72dbd0", "120.00");
+            var t = _processor.Purchase(_paymentMethodToken, "17.00");
 
-            Assert.IsTrue(trans.ProcessorResponse.Success);
+            Assert.IsTrue(t.ProcessorResponse.Success);
+            Assert.AreEqual(17, t.Amount);
+        }
+
+        [TestMethod]
+        public void Simple_Purchase_17_25_USD_As_Decimal()
+        {
+            var t = _processor.Purchase(_paymentMethodToken, 17.25m);
+
+            Assert.IsTrue(t.ProcessorResponse.Success);
+            Assert.AreEqual(17.25m, t.Amount);
+        }
+
+        [TestMethod]
+        public void Simple_Purchase_17_USD_As_Int()
+        {
+            var t = _processor.Purchase(_paymentMethodToken, 17);
+
+            Assert.IsTrue(t.ProcessorResponse.Success);
+            Assert.AreEqual(17, t.Amount);
+        }
+
+        [TestMethod]
+        public void Simple_Purchase_18_75_USD_As_Decimal_With_Descriptor_And_Custom()
+        {
+            string descriptor = _testDescriptor;
+            string custom = _testCustom;
+
+            var t = _processor.Purchase(_paymentMethodToken, 18.75m, descriptor, custom);
+
+            Assert.IsTrue(t.ProcessorResponse.Success);
+            Assert.AreEqual(18.75m, t.Amount);
+            Assert.AreEqual(descriptor, t.Descriptor);
+            Assert.AreEqual(custom, t.Custom);
+        }
+
+        [TestMethod]
+        public void Authorize_13_45_USD_As_Decimal()
+        {
+            decimal amount = 13.45m;
+            string descriptor = _testDescriptor;
+            string custom = _testCustom;
+
+            var t = _processor.Authorize(_paymentMethodToken, amount, descriptor, custom);
+
+            Assert.IsTrue(t.ProcessorResponse.Success);
+            Assert.AreEqual("authorize", t.TransactionType, true);
+            Assert.AreEqual(amount, t.Amount);
+            Assert.AreEqual(descriptor, t.Descriptor);
+            Assert.AreEqual(custom, t.Custom);
         }
     }
 }
