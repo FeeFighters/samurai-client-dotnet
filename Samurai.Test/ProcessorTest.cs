5ac894f1b92137fee4ea09d5c8c59652c56ac674
blackfoks <mail@blackfoks.com>
2011-08-09 13:17:33 +0600

    Processor test added, created a list for noREST tests.

---
+++ Samurai.Test/ProcessorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Samurai.Test
+{
+    [TestClass]
+    public class ProcessorTest
+    {
+        [TestMethod]
+        public void The_Processor_Test()
+        {
+            var theProcessor = Processor.TheProcessor;
+
+            Assert.AreEqual(Samurai.ProcessorToken, theProcessor.ProcessorToken);
+        }
+
+        [TestMethod]
+        public void New_Processor_Has_Given_Token()
+        {
+            var processor = new Processor("asdf");
+
+            Assert.AreEqual("asdf", processor.ProcessorToken);
+        }
+    }
+}
